a6fcdca0552de98016cdcb47f8d3a3d264034517
Franklin Rodriguez <frank14082013@gmail.com>
2022-09-30 11:40:42 -0600

    Se compila y ejecuta por primera vez

--- curso-fullstack/src/main/java/com/cursojava/curso/CursoApplication.java
+++ curso-fullstack/src/main/java/com/cursojava/curso/CursoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursoApplication.class, args);
+	public static void main( String[] args ) {
+		SpringApplication.run( CursoApplication.class, args );
 	}
 
 }
