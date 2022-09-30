49491a5f5d8d529160a72dfaba55c4e65a729e64
Franklin Rodriguez <frank14082013@gmail.com>
2022-09-30 13:51:24 -0600

    metodos a modo de prueba, no testing metodos para retornar 1 string una lista de string

--- curso-fullstack/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso-fullstack/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -3,6 +3,8 @@ package com.cursojava.curso.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 public class UsuarioController {
@@ -10,7 +12,13 @@ public class UsuarioController {
     @RequestMapping( value = "prueba" )
     public String prueba(){
         return "Prueba 1";
-    } // prueba
+    } // prueba 1
+
+    @RequestMapping( value = "prueba2" )
+    public List<String> prueba2(){
+        return List.of("manzana", "mandarina", "fresa");
+    } // prueba 2
+
 
 
 } // UsuarioController
