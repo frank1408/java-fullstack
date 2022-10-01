bb165d1a31ea75b36bbc05f97ac4f34a596ea1e9
Franklin Rodriguez <frank14082013@gmail.com>
2022-09-30 22:01:58 -0600

    pendiente

--- curso-fullstack/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso-fullstack/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,93 @@ public class UsuarioController {
     public List<String> prueba2(){
         return List.of("manzana", "mandarina", "fresa");
     } // prueba 2
+
+
+
+    @RequestMapping( value = "usuariofijo" )
+    public Usuario getUsuario(){
+        Usuario usuarioFijo = new Usuario();
+        usuarioFijo.setId("-1");
+        usuarioFijo.setNombre("Jennifer");
+        usuarioFijo.setApellido("Barrios");
+        usuarioFijo.setEmail("jbarrios@gmail.com");
+        usuarioFijo.setPassword("123456");
+        usuarioFijo.setTelefono("43215678");
+
+        return usuarioFijo;
+    } // public Usuario getUsuario
+
+
+
+
+    @RequestMapping( value = "usuariofijo/{id}" )
+    public Usuario getUsuarioId( @PathVariable String id ) {
+        Usuario usuarioFijo = new Usuario();
+        usuarioFijo.setId(id);
+        usuarioFijo.setNombre("Jennifer");
+        usuarioFijo.setApellido("Barrios");
+        usuarioFijo.setEmail("jbarrios@gmail.com");
+        usuarioFijo.setPassword("123456");
+        usuarioFijo.setTelefono("43215678");
+
+        return usuarioFijo;
+    } // public Usuario getUsuarioId
+
+
+
+
+
+
+    /*-----*/
+
+
+
+    @RequestMapping( value = "usuariofijo/update" )
+    public Usuario updateUsuario(){
+        Usuario usuarioFijo = new Usuario();
+        usuarioFijo.setNombre("Jennifer");
+        usuarioFijo.setApellido("Barrios");
+        usuarioFijo.setEmail("jbarrios@gmail.com");
+        usuarioFijo.setPassword("123456");
+        usuarioFijo.setTelefono("43215678");
+
+        return usuarioFijo;
+    } // public Usuario updateUsuario
+
+
+
+
+
+
+    @RequestMapping( value = "usuariofijo/delete" )
+    public Usuario deleteUsuario(){
+        Usuario usuarioFijo = new Usuario();
+        usuarioFijo.setNombre("Jennifer");
+        usuarioFijo.setApellido("Barrios");
+        usuarioFijo.setEmail("jbarrios@gmail.com");
+        usuarioFijo.setPassword("123456");
+        usuarioFijo.setTelefono("43215678");
+
+        return usuarioFijo;
+    } // public Usuario deleteUsuario
+
+
+
+
+
+
+
+    @RequestMapping( value = "usuariofijo/find" )
+    public Usuario buscarUsuario(){
+        Usuario usuarioFijo = new Usuario();
+        usuarioFijo.setNombre("Jennifer");
+        usuarioFijo.setApellido("Barrios");
+        usuarioFijo.setEmail("jbarrios@gmail.com");
+        usuarioFijo.setPassword("123456");
+        usuarioFijo.setTelefono("43215678");
+
+        return usuarioFijo;
+    } // public Usuario buscarUsuario
 
 
 
--- curso-fullstack/src/main/resources/static/js/demo/datatables-demo.js
+++ curso-fullstack/src/main/resources/static/js/demo/datatables-demo.js
@@ -1,4 +1,40 @@
+
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+cargarUsuarios();
+$('#dataTable').DataTable();
 });
+
+
+// error CORS
+// https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+
+
+
+async function cargarUsuarios() {
+
+const urlApi = "http://127.0.0.1:8080/usuariofijo";
+
+const objetoInfo = {
+
+method: "GET",
+
+headers: {
+"Accept": "application/json",
+"Content-Type": "application/json",
+"Access-Control-Allow-Origin": "*",
+"Access-Control-Allow-Methods": "GET",
+"Access-Control-Allow-Headers": "Content-Type, Authorization"
+}
+
+};
+
+const getRequest = await fetch( urlApi, objetoInfo );
+
+const resultadoUsuarios = await getRequest.json();
+
+//debugger;
+console.log( resultadoUsuarios );
+
+
+} // function cargarUsuarios
